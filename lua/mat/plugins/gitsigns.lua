13c99457dfca8b9fa7b637d442b7afadca49bd55
Matthew Truscott <matthew.truscott@turintech.ai>
2024-01-19 13:38:19 +0000

    feat: git blame

--- lua/mat/plugins/gitsigns.lua
+++ lua/mat/plugins/gitsigns.lua
@@ -1,5 +1,20 @@
 return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
-  config = true,
+  config = function()
+    local gitsigns = require("gitsigns")
+
+    gitsigns.setup({
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+        map('n', '<leader>gb', function() gs.blame_line { full = true } end)
+      end
+    })
+  end
+
 }
